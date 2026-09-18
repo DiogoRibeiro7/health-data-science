94cfe57c90c72ca665feffa70478c931f2973e19
Diogo Ribeiro <diogo_dj@hotmail.com>
2026-09-18 11:37:31 +0100

    test: cover internal metrics registry

--- tests/testthat/test_monitoring.R
+++ tests/testthat/test_monitoring.R
@@ -7,10 +7,9 @@ test_that("health_check returns ok by default", {
 })
 
 test_that("init_monitoring creates metrics", {
-  skip_if_not_installed("prometheus")
   m <- init_monitoring()
   expect_true(inherits(m$registry, "Registry"))
   record_metric(m$http_requests, labels = list(method = "GET", endpoint = "/"))
-  out <- prometheus::registry_render_metrics(m$registry)
+  out <- .hds_render_metrics(m$registry)
   expect_true(grepl("http_requests_total", out))
 })
